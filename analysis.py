fba8dcc7db7281059c2d4e9c579c3670b75ab827
aoconnor <oconnorails1@gmail.com>
2024-05-03 18:28:47 +0100

    Created functions for all the plots.  Addded some stats to the scatterplots, short note abt linear reg. added under plot

--- analysis.py
+++ analysis.py
@@ -4,62 +4,76 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sb
+import scipy
 
 ###################################
 # Load the data set
 df = pd.read_csv("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv")
 
-###################################
+###############################  1. Summary  of each variable  ###################################  
 # Print the summary statistics of all the variables of the data set
-print(df.describe())
-print(df.info())
+# print(df.describe())
+# print(df.info())
 
-###################################
-#Create a dataframe with only the numeric variables:
-df_numeric = df.select_dtypes(include = ('number'))
-#View the correlations between the continuous, numeric variables 
-print(df_numeric.corr())
+###############################  2. Histogram of each variable  ###################################
 
-###################################
-#Save a histogram of each numeric variable as a .png
+def histogram(x_value):
+    fig, ax = plt.subplots()
+    ax.hist(df[x_value], edgecolor = "black")
+    ax.set_xlabel(x_value)
+    ax.set_ylabel("Frequency")
+    ax.set_title(f"Distribution of {x_value} in Iris Data Set")
+    plt.savefig(f"Histogram {x_value}")
 
-###################################
-#Histogram of Sepal Length
-fig, ax = plt.subplots()
-ax.hist(df_numeric["sepal_length"], edgecolor = "black")
-ax.set_xlabel("Sepal Length (mm)")
-ax.set_ylabel("Frequency")
-ax.set_title("Distribution of Sepal Length in Iris Data Set")
-plt.savefig("Histogram Sepal Length")
+histogram("sepal_length")
+histogram("sepal_width")
+histogram("petal_length")
+histogram("petal_width")
 
-###################################
-#Histogram of Sepal Width
-fig, ax = plt.subplots()
-ax.hist(df_numeric["sepal_width"], edgecolor = "black")
-ax.set_xlabel("Sepal Width (mm)")
-ax.set_ylabel("Frequency")
-ax.set_title("Distribution of Sepal Width in Iris Data Set")
-plt.savefig("Histogram Sepal Width")
+###############################  2. Scatterplot of each variable  ###################################
 
-###################################
-#Histogram of Petal Length 
-fig, ax = plt.subplots()
-ax.hist(df_numeric["petal_length"], edgecolor = "black")
-ax.set_xlabel("Petal Length (mm)")
-ax.set_ylabel("Frequency")
-ax.set_title("Distribution of Petal Length in Iris Data Set")
-plt.savefig("Histogram Petal Length")
+correlation_matrix = df.select_dtypes(include = ('number')).corr() #Returns a correlation matrix with the R values for the correlation between each of the numeric values
 
-###################################
-#Histogram of Petal Width 
-fig, ax = plt.subplots()
-ax.hist(df_numeric["petal_width"], edgecolor = "black")
-ax.set_xlabel("Petal Width (mm)")
-ax.set_ylabel("Frequency")
-ax.set_title("Distribution of Petal Width in Iris Data Set")
-plt.savefig("Histogram Petal Width")
+def scatterplot(x_value, y_value, colour):
+    m, c = np.polyfit(                 #Identify the best fit line, y = mx+c, for x vs y, using Numpy's polyfit to do a least squares fit determine m and c
+            x = df[x_value], 
+            y = df[y_value], 
+            deg = 1                    #Degree of the polynomial 
+                      ) 
+    r_value = correlation_matrix.loc[x_value, y_value] #Get the regression fit from the correlation matrix 
 
-###################################
+    fig, ax = plt.subplots() # subplot() creates two variables, a matplotlib figure and axis. Assign variable names ax and fig
+    fig.subplots_adjust(bottom = 0.2)
+    scatterplot = ax.scatter(df[x_value], df[y_value], 
+                            c = df[colour], #color the scatterplot points by petal width
+                            cmap = "viridis") # specify the color map
+    
+    ax.plot(df[x_value], m*df[x_value]+c, c = "red", label = f"Linear Regression Fit, R = {r_value:.2f}") # plot the least squares fit line identified in the numpy polyfit 
+                                                                                                                      # x value will still be sepal length, y value will be m*x + c
+    
+    ax.set_xlabel(x_value)
+    ax.set_ylabel(y_value)
+    ax.set_title(f"{x_value} vs. {y_value} coloured by {colour}")
+    fig.colorbar(scatterplot, label = colour)
+    ax.legend()
+    
+    slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(df[x_value], df[y_value])
+    r_square = r_value**2
+    string = (f"The observed significance probability for the regression fit of {x_value} and {y_value} is {p_value:.2g}.\nThe R squared is {r_square:.2f}.")
+
+    plt.figtext(0,0, string, wrap=True, bbox=dict(boxstyle="round", #Matplotlib documentation: https://matplotlib.org/stable/gallery/text_labels_and_annotations/fancytextbox_demo.html#sphx-glr-gallery-text-labels-and-annotations-fancytextbox-demo-py
+                   ec=(1., 0.5, 0.5),
+                   fc=(1., 0.8, 0.8)))
+    plt.savefig(f"Scatterplot of {x_value} and {y_value} ", bbox_inches = 'tight') #To prevent savefig cropping the edges, use bbox_inches = tight, https://stackoverflow.com/questions/37427362/plt-show-shows-full-graph-but-savefig-is-cropping-the-image
+
+scatterplot("sepal_length", "petal_length", "petal_width")
+scatterplot("sepal_length", "petal_width", "petal_length")
+scatterplot("sepal_length", "sepal_width", "petal_width")
+scatterplot("petal_length", "petal_width", "sepal_length")
+scatterplot("sepal_width", "petal_width", "petal_length")
+scatterplot("sepal_width", "petal_length", "petal_width")
+
+###############################  Barchart of each variable by Species  ###################################
 #Plot a bar chart of mean Sepal  length, Sepal Width, Petal Width and Petal length by species on the same plot
 species = list(df["species"].unique()) # returns a np array with the three species in the data frame
 species_means = {
@@ -84,61 +98,4 @@ ax.set_title('Iris Sepal and Petal Length and Width by Species') # Set title of 
 ax.set_xticks(x + 0.3, species) # Labels and ticks on the x axis, offset so that the ticks sit in the middle of the 4 bars
 fig.legend(loc = "upper right", bbox_to_anchor=(-0.13, 0.38, 0.5, 0.5)) # Setting the location of the legend outside of the plot.
 plt.savefig("Barchart by Species")
-
-###################################
-# Save a scatterplot of of each pair of variables
-
-# Python tutorial on use of Seaborn https://realpython.com/python-seaborn/
-corr_pairplot = sb.pairplot(df, hue="species", diag_kind="hist").legend.set_title("Species")
-plt.savefig("seaborn")
-
-###################################
-# Scatterplot of Sepal Length vs Petal length, with a best fit line and coloured by Petal Width
-# Identify the best fit line, y = mx+c, for sepal length vs petal length
-# Use Numpy's polyfit to do a least squares fit determine m and c
-m, c = np.polyfit(
-        x = df["sepal_length"], 
-        y = df["petal_length"], 
-        deg = 1 #Degree of the polynomial 
-                  ) 
-fig, ax = plt.subplots() # subplot() creates two variables, a matplotlib figure and axis. Assign variable names ax and fig
-scatterplot = ax.scatter(df["sepal_length"], df_numeric["petal_length"], 
-                        c = df["petal_width"], #color the scatterplot points by petal width
-                        cmap = "viridis") # specify the color map
-
-ax.plot(df["sepal_length"], m*df_numeric["sepal_length"]+c, c = "red", label = "Linear Regression Fit, R = 0.87") # plot the least squares fit line identified in the numpy polyfit 
-                                                                                                                  # x value will still be sepal length, y value will be m*x + c
-
-ax.set_xlabel("Sepal Length (mm)")
-ax.set_ylabel("Petal Length (mm)")
-ax.set_title("Sepal Length vs. Petal Length in Iris Flowers")
-fig.colorbar(scatterplot, label = "Petal Length")
-ax.legend()
-
-plt.savefig("Scatterplot Sepal Length vs Petal Length")
-
-###################################
-# Scatterplot of Petal Width vs Petal length, with a best fit line and coloured by Sepal Length
-m, c = np.polyfit(
-        x = df["petal_width"], 
-        y = df["petal_length"], 
-        deg = 1 #Degree of the polynomial 
-                  ) 
-fig, ax = plt.subplots() # subplot() creates two variables, a matplotlib figure and axis. Assign variable names ax and fig
-scatterplot = ax.scatter(df["petal_width"], df_numeric["petal_length"], 
-                        c = df["sepal_length"], #color the scatterplot points by petal width
-                        cmap = "viridis") # specify the color map
-
-
-# plot the least squares fit line identified in the numpy polyfit 
-# x value will still be body mass, y value will be m*x + c
-ax.plot(df["petal_width"], m*df_numeric["petal_width"]+c, c = "red", label = "Linear Regression Fit, R = 0.96")
-
-ax.set_xlabel("Petal Width (mm)")
-ax.set_ylabel("Petal Length (mm)")
-ax.set_title("Petal Width vs. Petal Length in Iris Flowers")
-fig.colorbar(scatterplot, label = "Sepal Length")
-ax.legend()
-
-plt.savefig("Scatterplot Petal Width vs Petal Length")
 
